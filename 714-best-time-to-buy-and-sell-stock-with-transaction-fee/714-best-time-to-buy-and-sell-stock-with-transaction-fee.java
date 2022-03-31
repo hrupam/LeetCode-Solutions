f3644305852c01ad4b1c3833b0aa5891087d8803
Rupam Hari <hrupam55@gmail.com>
2022-03-31 16:01:34 +0530

    Time: 33 ms (19.08%), Space: 74.9 MB (44.17%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -7,17 +7,13 @@ class Solution {
         dp[n][0] = dp[n][1] = 0;
 
         for (int ind = n - 1; ind >= 0; ind--) {
-            for (int canBuy = 0; canBuy <= 1; canBuy++) {
 
-                int profit = 0;
+            dp[ind][1] = Math.max(-arr[ind] + dp[ind + 1][0], dp[ind + 1][1]);
 
-                if (canBuy == 1)
-                    dp[ind][1] = Math.max(-arr[ind] + dp[ind + 1][0], dp[ind + 1][1]);
-
-                else
-                    dp[ind][0] = Math.max((arr[ind] - fee) + dp[ind + 1][1], dp[ind + 1][0]);
-            }
+            dp[ind][0] = Math.max((arr[ind] - fee) + dp[ind + 1][1], dp[ind + 1][0]);
+            
         }
+        
 
         return dp[0][1];
     }
