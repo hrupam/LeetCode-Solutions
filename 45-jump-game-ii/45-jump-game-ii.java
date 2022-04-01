5541fd2db48f8d01afe4176f85aada3686e4e94f
Rupam Hari <hrupam55@gmail.com>
2022-04-02 01:57:52 +0530

    Time: 65 ms (28.57%), Space: 49.9 MB (26.68%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
+        
         int n=nums.length;
         int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return f(0,nums,n,dp);
-    }
-    
-    private static int f(int ind, int[] a, int n, int[] dp){
-        if(ind==n-1) return 0;
-        if(dp[ind]!=-1) return dp[ind];
         
-        int mini=(int)1e9;
-        for(int j=1;j<=a[ind];j++){
-            if(ind+j<n)
-                mini=Math.min(mini, 1+f(ind+j,a,n,dp));
+        for(int ind=n-2;ind>=0;ind--){
+            int mini=(int)1e9;
+            for(int j=1;j<=nums[ind];j++){
+                if(ind+j<n) mini=Math.min(mini, 1+dp[ind+j]);
+            }
+            
+            dp[ind]=mini;
         }
-        return dp[ind]=mini;
+        
+        return dp[0];
     }
 }
