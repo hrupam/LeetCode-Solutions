80d42558ee1229997cd4aa13b6d44778c4df6ab5
Rupam Hari <hrupam55@gmail.com>
2022-04-02 12:49:43 +0530

    Time: 13 ms (28.84%), Space: 54.7 MB (41.50%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r))
+                return (isPalin(s,l+1,r)) | (isPalin(s,l,r-1));
+
+            l++; r--;
+        }
+        return true;
+    }
+    
+    static boolean isPalin(String s, int l, int r){
+        while(l<r)
+            if(s.charAt(l++)!=s.charAt(r--)) return false;
+        
+        return true;
+    }
+}
