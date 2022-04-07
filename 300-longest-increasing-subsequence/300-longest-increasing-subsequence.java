eda1f790548c41f212044232aaca8b803c0e84ab
Rupam Hari <hrupam55@gmail.com>
2022-04-07 20:49:53 +0530

    Time: 223 ms (5.39%), Space: 243.5 MB (5.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -4,10 +4,6 @@ class Solution {
         
         int[][] dp=new int[n+1][n+1];
         
-        //for(int[] x:dp) Arrays.fill(x,-1);
-        
-        // return f(0,-1,nums,n,dp);
-        
         for(int i=n-1;i>=0;i--){
             for(int lasti=i-1;lasti>=-1;lasti--){
                 
@@ -20,18 +16,5 @@ class Solution {
         }
         
         return dp[0][0];
-    }
-    
-    static int f(int i, int lasti, int[] a, int n, int[][] dp){ 
-        if(i==n) return 0;
-        
-        if(dp[i][lasti+1]!=-1) return dp[i][lasti+1];
-        
-        int p=0;
-        if(lasti==-1 || a[i]>a[lasti]) p=1+f(i+1,i, a,n,dp);
-        int np=f(i+1,lasti, a,n,dp);
-        
-        return dp[i][lasti+1]=Math.max(p,np);
-        
     }
 }
