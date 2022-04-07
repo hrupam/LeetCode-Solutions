0f8ad0c92d72dce7b22f6979d38b2c74a1fdd96d
Rupam Hari <hrupam55@gmail.com>
2022-04-07 18:03:26 +0530

    Time: 3 ms (35.54%), Space: 39.3 MB (99.46%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        List<Integer> list=IntStream.of(stones).boxed().collect(Collectors.toList());
+        
+        while(list.size()>1){
+            Collections.sort(list, Comparator.reverseOrder());
+            int i=0; int j=1;
+            int x=list.get(j);
+            int y=list.get(i);
+            
+            if(x==y){
+                list.remove(j); list.remove(i);
+            }
+            else{
+                list.remove(j);
+                list.set(i, y-x);
+            }
+        }
+        if(list.size()==1) return list.get(0);
+        return 0;
+    }
+}
