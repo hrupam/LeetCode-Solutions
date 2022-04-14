3cfe9b0ab8de9eca3d2c2ef3b307caebbe6037e8
Rupam Hari <hrupam55@gmail.com>
2022-04-14 23:17:55 +0530

    Time: 15 ms (13.49%), Space: 51.9 MB (6.45%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -5,17 +5,13 @@ class Solution {
         int p=s3.length();
         
         if(n+m!=p) return false;
-        
-        char[] s11=s1.toCharArray();
-        char[] s22=s2.toCharArray();
-        char[] s33=s3.toCharArray();
 
         int[][][] dp=new int[n][m][p];
         for(int[][] x:dp) for(int[] y:x) Arrays.fill(y,-1);
         
-        int res=f(n-1,m-1,p-1,s11,s22,s33,dp);
-        if(res==1) return true;
-        return false;
+        int res=f(n-1,m-1,p-1,s1.toCharArray(),s2.toCharArray(),s3.toCharArray(),dp);
+        
+        return res==1;
     }
     
     private static int f(int i, int j, int k, char[] s1, char[] s2, char[] s3, int[][][] dp){
@@ -23,6 +19,7 @@ class Solution {
             if(k<0) return 1;
             return 0;
         }
+        
         if(i<0){
             while(j>=0 && k>=0){
                 if(s2[j]!=s3[k]) return 0;
@@ -31,6 +28,7 @@ class Solution {
             if(j<0 && k<0) return 1;
             return 0;
         }
+        
         if(j<0){
             while(i>=0 && k>=0){
                 if(s1[i]!=s3[k]) return 0;
@@ -39,16 +37,22 @@ class Solution {
             if(i<0 && k<0) return 1;
             return 0;
         }
+        
         if(k<0){
             if(i>=0) return 0;
             if(j>=0) return 0;
             return 1;
         }
+        
         if(dp[i][j][k]!=-1) return dp[i][j][k];
         
-        if(s1[i]==s3[k] && s2[j]==s3[k]) return dp[i][j][k]=f(i-1,j,k-1,s1,s2,s3,dp) | f(i,j-1,k-1,s1,s2,s3,dp);
+        if(s1[i]==s3[k] && s2[j]==s3[k])
+            return dp[i][j][k]=f(i-1,j,k-1,s1,s2,s3,dp) | f(i,j-1,k-1,s1,s2,s3,dp);
+        
         if(s1[i]==s3[k]) return dp[i][j][k]=f(i-1,j,k-1,s1,s2,s3,dp);
-        else if(s2[j]==s3[k]) return dp[i][j][k]=f(i,j-1,k-1,s1,s2,s3,dp);
+        
+        if(s2[j]==s3[k]) return dp[i][j][k]=f(i,j-1,k-1,s1,s2,s3,dp);
+        
         return dp[i][j][k]=0;
     }
 }
