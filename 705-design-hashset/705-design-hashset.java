bd8a3131ff0ab8e696bdcced569a4e541554401c
Rupam Hari <hrupam55@gmail.com>
2022-04-21 12:34:39 +0530

    Time: 13 ms (92.01%), Space: 49.6 MB (91.88%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,11 @@
+class MyHashSet extends HashSet<Integer> {
+    //HashSet will do the rest \U0001f61d\U0001f61d\U0001f61d
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
