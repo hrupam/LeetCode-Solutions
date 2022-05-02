dbd4aa5d0345957e0d8a7991d5c9c18455150182
Rupam Hari <hrupam55@gmail.com>
2022-05-02 22:04:32 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.53%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int fib(int n) {
-        return f(n);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
     }
     
-    private static int f(int n){
+    private static int f(int n, int[] dp){
         if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
         
-        return f(n-1)+f(n-2);
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
     }
 }
