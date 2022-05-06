f8bcf49ef5bba7a1d7d21fb7e959ef7251a0dfda
Rupam Hari <hrupam55@gmail.com>
2022-05-06 21:55:09 +0530

    Time: 1548 ms (7.59%), Space: 41.9 MB (91.83%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int minCut(String s) {
         int n=s.length();
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        int[] dp=new int[n+1];
         
-        return f(0,s,n,dp)-1;
-    }
-    
-    static int f(int i, String s, int n, int[] dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        
-        int mini=(int)1e9;
-        for(int j=i;j<n;j++){
-            if(isPalindrome(s,i,j)){
-                int cut=1+f(j+1,s,n,dp);
-                mini=Math.min(mini,cut);
+        for(int i=n-1;i>=0;i--){
+            int mini=(int)1e9;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)){
+                    int cut=1+dp[j+1];
+                    mini=Math.min(mini,cut);
+                }
             }
+            dp[i]=mini;
         }
-        return dp[i]=mini;
+        
+        return dp[0]-1;
     }
     
     static boolean isPalindrome(String s, int l, int r){
