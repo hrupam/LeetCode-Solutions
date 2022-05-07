3ab76d44cfd8d94a8b2096a07ac1ce4cf36d1fbb
Rupam Hari <hrupam55@gmail.com>
2022-05-07 21:27:00 +0530

    Time: 8 ms (57.28%), Space: 43.3 MB (15.92%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,22 +1,17 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n=arr.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        int[] dp=new int[n+1];
         
-        return f(0,arr,n,k,dp);
-    }
-    
-    private static int f(int i, int[] arr, int n, int k, int[] dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        
-        int maxNo=arr[i]; int maxi=(int)-1e9;
-        for(int j=i;j<i+k && j<n;j++){
-            maxNo=Math.max(maxNo,arr[j]);
-            int cost=(j-i+1)*maxNo + f(j+1,arr,n,k,dp);
-            maxi=Math.max(maxi,cost);
+        for(int i=n-1;i>=0;i--){
+            int maxNo=arr[i]; int maxi=(int)-1e9;
+            for(int j=i;j<i+k && j<n;j++){
+                maxNo=Math.max(maxNo,arr[j]);
+                int cost=(j-i+1)*maxNo + dp[j+1];
+                maxi=Math.max(maxi,cost);
+            }
+            dp[i]=maxi;  
         }
-        return dp[i]=maxi;    
+        return dp[0];
     }
 }
