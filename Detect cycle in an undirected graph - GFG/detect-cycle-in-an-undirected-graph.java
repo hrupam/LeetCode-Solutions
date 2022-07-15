4c2698230052250d4dd8730e39e2d6166123764b
Rupam Hari <hrupam55@gmail.com>
2022-07-15 22:21:08 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,44 +31,25 @@ class GFG {
 }// } Driver Code Ends
 
 class Solution {
-    
-    static class Pair{
-        int first;
-        int second;
-        public Pair(int first, int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
-    
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean[] vis=new boolean[V];
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(hasCycle(i,vis,adj)) return true;
+                if(hasCycle(i,-1,vis,adj)) return true;
             }
         }
         return false;
     }
     
-    private static boolean hasCycle(int i, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
-        Deque<Pair> queue=new ArrayDeque<>();
+    private static boolean hasCycle(int i, int prev, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
         vis[i]=true;
-        queue.offerLast(new Pair(i,-1));
-        while(!queue.isEmpty()){
-            Pair pair=queue.pollFirst();
-            int node=pair.first;
-            int prev=pair.second;
-            
-            for(int j: adj.get(node)){
-                if(!vis[j]){
-                    queue.offerLast(new Pair(j,node));
-                    vis[j]=true;
-                }
-                else if(j!=prev) return true;  
-            }
+        for(int j: adj.get(i)){
+             if(!vis[j]){
+                 if(hasCycle(j,i,vis,adj)) return true;
+             }
+             else if(prev!=j) return true;
         }
         return false;
     }
