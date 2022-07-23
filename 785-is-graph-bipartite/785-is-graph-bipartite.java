2f2b2c1e9f279dc931e27dfe5e6150b0525a8c48
Rupam Hari <hrupam55@gmail.com>
2022-07-23 23:33:50 +0530

    Time: 1 ms (88.90%), Space: 54.7 MB (5.32%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,26 +1,26 @@
 class Solution {
+
     public boolean isBipartite(int[][] graph) {
-        int n=graph.length;
-        int[] color=new int[n];
-        Arrays.fill(color,-1);
-        
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(!f(i,color,graph)) return false;
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!f(i, color, graph)) return false;
             }
         }
         return true;
     }
-    
-    private static boolean f(int i, int[] color, int[][] adj){
-        if(color[i]==-1) color[i]=0;
-        
-        for(int j: adj[i]){
-            if(color[j]==-1){
-                color[j]=1-color[i];
-                if(!f(j,color,adj)) return false;
-            }
-            else if(color[j]==color[i]) return false;
+
+    private static boolean f(int i, int[] color, int[][] adj) {
+        if (color[i] == -1) color[i] = 0;
+
+        for (int j : adj[i]) {
+            if (color[j] == -1) {
+                color[j] = 1 - color[i];
+                if (!f(j, color, adj)) return false;
+            } else if (color[j] == color[i]) return false;
         }
         return true;
     }
