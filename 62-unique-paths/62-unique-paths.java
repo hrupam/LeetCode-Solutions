43c8e0d282b022269f762a395d30e86a4a0e167a
Rupam Hari <hrupam55@gmail.com>
2022-08-01 12:02:18 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (57.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -3,12 +3,13 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for (int[] x : dp) Arrays.fill(x, -1);
+        
 
         return f(0, 0, m, n, dp);
     }
 
     private int f(int i, int j, int m, int n, int[][] dp) {
-        if (i >= m || j >= n) return 0;
+        if (i == m || j == n) return 0;
         if (i == m - 1 && j == n - 1) return 1;
         if (dp[i][j] != -1) return dp[i][j];
 
