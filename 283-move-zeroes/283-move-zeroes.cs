0646bcf78f1272456c79b71f75e5e0d76e94a51d
Rupam Hari <hrupam55@gmail.com>
2022-08-04 21:59:49 +0530

    Time: 301 ms (21.55%), Space: 47 MB (56.75%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cs
+++ 283-move-zeroes/283-move-zeroes.cs
@@ -1,14 +1,11 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
         int n=nums.Length;
-        int right=0; int left=0;
+        int left=0; int right=0;
+        
         while(right<n){
-            if(nums[right]==0) right++;
-            else{
-                Swap(left,right,nums);
-                left++;
-                right++;
-            }
+            if(nums[right]!=0) Swap(left++,right,nums);
+            right++;
         }
     }
     
