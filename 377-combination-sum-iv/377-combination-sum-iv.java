364c536187067a617a9c021c06970ad5a26a89e1
Rupam Hari <hrupam55@gmail.com>
2022-08-05 09:49:55 +0530

    Time: 2 ms (65.18%), Space: 41.2 MB (73.08%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -3,19 +3,17 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         int n = nums.length;
         int[] dp = new int[target + 1];
-        Arrays.fill(dp, -1);
 
-        return f(target, nums, n, dp);
-    }
+        dp[0] = 1;
 
-    private static int f(int target, int[] nums, int n, int[] dp) {
-        if (target < 0) return 0;
-        if (target == 0) return 1;
-        if (dp[target] != -1) return dp[target];
+        for (int tar = 1; tar <= target; tar++) {
+            int ctr = 0;
+            for (int i = 0; i < n; i++) {
+                if (tar >= nums[i]) ctr += dp[tar - nums[i]];
+            }
+            dp[tar] = ctr;
+        }
 
-        int ctr = 0;
-        for (int i = 0; i < n; i++) ctr += f(target - nums[i], nums, n, dp);
-
-        return dp[target] = ctr;
+        return dp[target];
     }
 }
