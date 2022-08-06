60d569a0b0158ec42a04ea1ea2e45c8ea0ad3d90
Rupam Hari <hrupam55@gmail.com>
2022-08-07 00:57:34 +0530

    Time: 11 ms (46.06%), Space: 53.6 MB (13.89%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -7,7 +7,6 @@ class Solution {
 
     private static boolean f(int l, int r, char[] str) {
         if (l >= r) return true;
-        if (!Character.isLetterOrDigit(str[l]) && !Character.isLetterOrDigit(str[r])) return f(l + 1, r - 1, str);
         if (!Character.isLetterOrDigit(str[l])) return f(l + 1, r, str);
         if (!Character.isLetterOrDigit(str[r])) return f(l, r - 1, str);
         if (Character.toLowerCase(str[l]) != Character.toLowerCase(str[r])) return false;
