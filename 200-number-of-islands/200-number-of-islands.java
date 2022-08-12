1f4ff34bad8f8623045c9661ef1989cac5131564
Rupam Hari <hrupam55@gmail.com>
2022-08-12 13:14:06 +0530

    Time: 9 ms (24.15%), Space: 56.7 MB (72.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -40,14 +40,14 @@ class Solution {
             }
         }
     }
-}
 
-class Pair {
-    int first;
-    int second;
+    class Pair {
+        int first;
+        int second;
 
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
     }
 }
