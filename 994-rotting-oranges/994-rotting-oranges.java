bced90eb9ab35c34bf563eb3c08d18a0ceb5e53a
Rupam Hari <hrupam55@gmail.com>
2022-08-14 01:07:27 +0530

    Time: 5 ms (26.12%), Space: 43.5 MB (22.45%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<Pair> queue = new LinkedList<>();
+        int freshCtr = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) queue.add(new Pair(i, j, 0));
+                if (grid[i][j] == 1) freshCtr++;
+            }
+        }
+        int minutes = 0;
+        while (!queue.isEmpty()) {
+            Pair p = queue.poll();
+            int i = p.first;
+            int j = p.second;
+            int time = p.time;
+            minutes = Math.max(minutes, time);
+
+            int[][] deviations = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+            for (int[] d : deviations) {
+                int ni = i + d[0];
+                int nj = j + d[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1) {
+                    queue.add(new Pair(ni, nj, time + 1));
+                    grid[ni][nj] = 2;
+                    --freshCtr;
+                }
+            }
+        }
+        return freshCtr == 0 ? minutes : -1;
+    }
+}
+
+class Pair {
+    int first;
+    int second;
+    int time;
+
+    public Pair(int first, int second, int time) {
+        this.first = first;
+        this.second = second;
+        this.time = time;
+    }
+}
